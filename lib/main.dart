d1c4a5d057b9c9c90289f7e6f0fd921e06905b2c
Iñaki Silanes <isilanes@gmail.com>
2023-08-12 22:06:24 +0200

    Rewrite to start looking like what I want to build

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,55 +10,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "Startup Name Generator",
-      home: RandomWords(),
+      title: "biblio-f",
+      home: BookListState(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({super.key});
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
+List<String> getBooks() {
+  return ["a", "b", "c", "d"];
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
+class BookListState extends StatefulWidget {
+  const BookListState({super.key});
+
+  @override
+  State<BookListState> createState() => _BooksState();
+}
+
+class _BooksState extends State<BookListState> {
+  final _biggerFont = const TextStyle(fontSize: 24);
 
   @override
   Widget build(BuildContext context){
-    // final wordPair = WordPair.random();
-    // return Text(wordPair.asPascalCase);
-
     return Scaffold(
       appBar: AppBar(
-        title: Text("Startup Name Generator"),
+        title: const Text("biblio"),
       ),
-      body: _buildSuggestions(),
+      body: _bookList(),
     );
   }
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (BuildContext context, int i) {
-        if (i.isOdd) return const Divider();
-
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
+  Widget _bookList() {
+    return ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          for (final book in getBooks())
+            _buildRow(book)
+        ],
     );
   }
 
-  Widget _buildRow(WordPair pair) {
+  Widget _buildRow(String title) {
     return ListTile(
+      leading: const Icon(Icons.book, color: Colors.cyan),
       title: Text(
-        pair.asPascalCase,
+        title,
         style: _biggerFont,
       ),
     );
